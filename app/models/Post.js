24411f536ed33c8f080ba9910f5431f7b26234b4
ashsic <99445200+ashsic@users.noreply.github.com>
2024-03-21 18:05:38 -0400

    Post schema

--- app/models/Post.js
+++ app/models/Post.js
@@ -1,9 +1,73 @@
 import mongoose from 'mongoose';
 
 const postSchema = new mongoose.Schema({
-  participants: [{
+  poster: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User' 
+  },
+
+  content: {
+    type: String,
+    required: true
+  },
+
+  timestamp: {
+    type: Date,
+    required: true
+  },
+
+  likes: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+
+  comments: [{
+    commenter: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User' 
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    timestamp: {
+      type: Date,
+      required: true
+    },
+    likes: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    replies: [{
+      replier: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User' 
+      },
+      content: {
+        type: String,
+        required: true
+      },
+      timestamp: {
+        type: Date,
+        required: true
+      },
+      likes: {
+        type: Number,
+        required: true,
+        default: 0
+      },
+    }]
   }],
-  messages: []
+
+  images: {
+    type: [Image],
+    validate: {
+      validator: function(arr) {
+          return arr.length <= 4;
+      },
+      message: 'Limit of 4 images to a post'
+    }
+  }
 });

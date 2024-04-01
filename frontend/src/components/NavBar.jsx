4400a7008b794c15d81379aeb46d444d59e702cb
ashsic <99445200+ashsic@users.noreply.github.com>
2024-04-01 12:24:27 -0400

    styling navbar and conversations

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,52 +4,68 @@ export default function NavBar() {
   const navItems = [
     {
       name: "Home",
-      route: "/"
+      route: "/",
+      icon: "home"
     },
     {
       name: "Search",
-      route: "/search"
+      route: "/search",
+      icon: "search"
     },
     {
       name: "Messages",
-      route: "/messages"
+      route: "/messages",
+      icon: "message"
     },
     {
       name: "Create",
-      route: "/create"
+      route: "/create",
+      icon: "add_box"
     },
     {
       name: "Notifications",
-      route: "/notifications"
+      route: "/notifications",
+      icon: "notifications"
     },
     {
       name: "Profile",
-      route: "/profile"
+      route: "/profile",
+      icon: "person"
     }
   ];
 
   return (
-    <nav className=" col-auto w-20 lg:w-60 2xl:w-80 flex flex-col h-screen border-r border-gray-500 p-6">
+    <nav className="col-auto w-20 lg:w-60 2xl:w-80 flex flex-col h-screen border-r border-gray-500 p-3">
+      <h1 className="text-2xl p-3 hidden lg:block">Website Name</h1>
       <ul className="flex flex-1 flex-col">
         {navItems.map((item) => {
           return (
             <li 
-            className={item.route === "/" ? "m-4 text-2xl" : "m-4 text-xl"}
+            className="h-14 p-3 text-lg flex items-center"
             key={item.name}>
               <a href={item.route}>
-                {/* icon */}
-                <p className="hidden lg:block">
-                  {item.name}
-                </p>
+                <div className="flex w-32">
+                  <i className="material-icons h-6">{item.icon}</i>
+                  <p className="hidden h-6 ml-3 items-center lg:flex">
+                    {item.name}
+                  </p>
+                </div>
               </a>
             </li>
           );
         })}
       </ul>
 
-      <p className="flex m-4 text-xl">
-        <a className="hidden lg:block" href="/settings">Settings</a>
-      </p>
+      <div className="h-14 p-3 text-lg flex items-center">
+        <a href="/settings">
+          <div className="flex w-32">
+            <i className="material-icons h-6">settings</i>
+            <p className="hidden h-6 ml-3 items-center lg:flex">
+              Settings
+            </p>
+          </div>
+        </a>
+      </div>
     </nav>
   );
 };
